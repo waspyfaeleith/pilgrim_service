e9154a5866423be42045f36385a643a29e2c4612
Sandy McMillan <sandy.mcmillan@codeclan.com>
2018-12-18 15:28:25 +0000

    added boilerplate for frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        Welcome!
+      </Fragment>
+    );
+  }
+}
+
+export default App;
