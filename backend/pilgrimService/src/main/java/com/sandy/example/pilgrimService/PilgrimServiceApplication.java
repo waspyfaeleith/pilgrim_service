0aa02e4b0d0c7d73216751bb8052259dda0aa955
Sandy McMillan <sandy_mcmillan@yahoo.co.uk>
2019-05-20 20:16:27 +0100

    added basic security

--- backend/pilgrimService/src/main/java/com/sandy/example/pilgrimService/PilgrimServiceApplication.java
+++ backend/pilgrimService/src/main/java/com/sandy/example/pilgrimService/PilgrimServiceApplication.java
@@ -2,8 +2,9 @@ package com.sandy.example.pilgrimService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class PilgrimServiceApplication {
 
 	public static void main(String[] args) {
